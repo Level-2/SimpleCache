e83421b20ca2ed6135187aa980c375c2185ce05b
Tom Butler <tom@r.je>
2018-04-21 17:57:56 +0100

    Added garbage collection to clean up old files

--- SimpleCache.php
+++ SimpleCache.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* @description     A very simple filesystem cache class that stores one entry per file        *
  * @author          Tom Butler tom@r.je                                                        *
  * @copyright       2015 Tom Butler                                                            *
@@ -7,25 +7,41 @@
 namespace SimpleCache;
 class SimpleCache implements \ArrayAccess {
 	private $dir;
-	
-	public function __construct($dir) {
+
+	public function __construct($dir, $maxAge = null, $gcInterval = 86400) {
 		$this->dir = rtrim(realpath($dir), \DIRECTORY_SEPARATOR);
+		$this->gc($maxAge, $gcInterval);
 	}
-	
+
 	public function offsetSet($key, $value) {
 		file_put_contents($this->dir . \DIRECTORY_SEPARATOR . $key, serialize($value));
 	}
 
 	public function offsetGet($key) {
+		touch(file_get_contents($this->dir . \DIRECTORY_SEPARATOR . $key));
 		if ($this->offsetExists($key)) return unserialize(file_get_contents($this->dir . \DIRECTORY_SEPARATOR . $key));
 		else return null;
 	}
-	
+
 	public function offsetExists($key) {
 		return is_file($this->dir . \DIRECTORY_SEPARATOR . $key);
 	}
-	
+
 	public function offsetUnset($key) {
 		unlink($this->dir . \DIRECTORY_SEPARATOR . $key);
+	}
+
+	private function gc($maxAge, $gcInterval) {
+		if ($maxAge && rand(1,100) == 1) {
+			$lastRun = $this['___cacheGC'];
+			if ($lastRun+$gcInterval < time()) {
+				foreach (new \DirectoryIterator($this->dir) as $file) {
+					if  (filemtime($this->dir . DIRECTORY_SEPARATOR . $file->getFileName()) + $maxAge < time()) {
+						unlink($this->dir . DIRECTORY_SEPARATOR . $file->getFileName());
+					}
+				}
+				$this['___cacheGC'] = time();
+			}
+		}
 	}
 }
